3ee685c1fe51b8dfe4b321bf03023bdadb7a776a
kaifuntwale2025comp-pixel <kaifuntwale2025.comp@mmcoe.edu.in>
2025-10-13 17:56:37 +0530

    Update assingment1.cpp

--- assingment1.cpp
+++ assingment1.cpp
@@ -1,85 +1,43 @@
 #include <iostream>
-#include <string>
 using namespace std;
-class bank {
-private:
-string name, acc_type;
-int acc_no;
-float balance;
-public:
-bank(string n, string at, int accnum, float bal) {
-   name = n;
-   acc_type = at;
-   acc_no = accnum;
-   balance = bal;
-}
 
-void display();
-void deposit();
-void withdraw();
+class division {
+    float a, b;
+public:
+    void accept();
+    void display();
 };
 
-void bank::display() {
-cout << "\n\nAccount details:\nName: " << name << endl;
-cout << "Account type: " << acc_type << endl;
-cout << "Account number: " << acc_no << endl;
-cout << "Bank acc balance: " << balance << endl;
+void division::accept() {
+    cout << "Enter first num: ";
+    cin >> a;
+    cout << "Enter second num: ";
+    cin >> b;
 }
-void bank::deposit() {
-float damount;
-cout << "\nEnter the amount you wish to deposit: ";
-cin >> damount;
-balance += damount;
-}
-void bank::withdraw() {
-float wamount;
-cout << "\nEnter amt you wish to withdraw: ";
-cin >> wamount;
-if (wamount<= balance) {
-balance -= wamount;
-} else {
-cout << "Insufficient bank account balance!" << endl;
-}
-}
-int main () {
-int choice;
 
-string name1, actp;
-int acnm;
-float blnc;
-      cout << "Enter details of your bank account\nEnter name: ";
-cin >> name1;
-cout << "Account type: ";
-cin >> actp;
-cout << "Enter acc number: ";
-cin >> acnm;
-cout << "Enter account balance: ";
-cin >> blnc;
-bank cust2 (name1, actp, acnm, blnc);
-do {
-cout << "\nMention the operation you wish to 1 for display,2 for withdraw,3 for deposite," << endl;
-cin >> choice;
-switch (choice) {
-case 1: {
-cust2.display();
-break;
+void division::display() {
+    try {
+        if (b == 0) {
+            throw "Div by zero not possible";
+        } else {
+            cout << "Result : " << a / b << endl;
+        }
+    }
+    catch (const char* msg) {
+        cout << msg << endl;
+        cout << "Enter a non-zero value: ";
+        cin >> b;
+        if (b != 0) {
+            cout << "Result after correction: " << a / b << endl;
+        } else {
+            cout << "Still zero, division not possible" << endl;
+        }
+    }
 }
-case 2: {
-cust2.withdraw();
-cust2.display();
-break;
-}
-case 3: {
-cust2.deposit();
-cust2.display();
-break;
-}
-case 4: {
-cout << "Thank you!!";
-break;
-}
-default: cout << "Error! Unidentified choice!" << endl;
-}
-} while (choice != 4);
-return 0;
+
+int main() {
+    division A;
+    A.accept();
+    A.display();
+    return 0;
 }
